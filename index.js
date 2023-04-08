3fde68abc840b26c4d04b4bf89f7fcbceadae28e
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-04-08 16:28:32 +0300

    added callback function for newDeckBtn click

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 const newDeckBtn = document.getElementById("new-deck-btn")
 
 const drawBtn = document.getElementById("draw-btn")
+
+let deckId 
+
+newDeckBtn.addEventListener("click", () => {
+    fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1",{method: "GET"})
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        deckId = data.deck_id
+        
+      })
+})
