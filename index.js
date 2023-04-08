1b4518d5d01a315c890e42b39b56d5de0892a735
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-04-08 16:50:38 +0300

    added callback function for drawBtn

--- index.js
+++ index.js
@@ -2,14 +2,23 @@ const newDeckBtn = document.getElementById("new-deck-btn")
 
 const drawBtn = document.getElementById("draw-btn")
 
-let deckId 
+let deckId
 
 newDeckBtn.addEventListener("click", () => {
-    fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1",{method: "GET"})
-      .then(response => response.json())
-      .then(data => {
-        console.log(data)
-        deckId = data.deck_id
-        
-      })
+    fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1", { method: "GET" })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            deckId = data.deck_id
+            console.log(data.deck_id)
+        })
+})
+
+
+drawBtn.addEventListener("click", () => {
+    console.log(deckId)
+    fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
+        .then(response => response.json())
+        .then(data => console.log(data)
+        )
 })
